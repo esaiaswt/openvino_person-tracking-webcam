8e86e38bb2a6d4441063ee2c1adfd9d481df6785
William Tan <51481300+esaiaswt@users.noreply.github.com>
2024-09-26 15:32:07 +0800

    Update app.py
    outside loop webcam

--- app.py
+++ app.py
@@ -432,9 +432,9 @@ source = cam_id if USE_WEBCAM else video_file
 
 #run_person_tracking(source=source, flip=USE_WEBCAM, use_popup=False)
 processing_times = collections.deque()
+image = camera_input_live()
 while True:
     # Grab the frame. 
-    image = camera_input_live()
     if image is not None:
         uploaded_image = PIL.Image.open(image)
         frame = cv2.cvtColor(numpy.array(uploaded_image), cv2.COLOR_RGB2BGR)
